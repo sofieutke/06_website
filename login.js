229a00df808b74e6adb39e378333f761b74a2a33
sofieutke <sobo0001@stud.kea.dk>
2024-12-12 14:24:52 +0100

    test

---
+++ login.js
@@ -0,0 +1,9 @@
+// document.querySelectorAll(".shopknap").forEach((button) => {
+//   button.addEventListener("click", function (event) {
+//     event.preventDefault(); // Forhindrer standard link-adfærd
+//     const targetPage = this.getAttribute("data-target"); // Hent værdien fra data-target
+//     if (targetPage) {
+//       window.location.href = targetPage; // Naviger til målsiden
+//     }
+//   });
+// });
